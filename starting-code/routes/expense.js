e1f9e730bdafdd8987b129eff21528f431c7dace
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-30 19:34:49 -0600

    Import more middleware

--- starting-code/routes/expense.js
+++ starting-code/routes/expense.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
-const create = require("../controllers");
+const { create, expenseById, read } = require("../controllers");
 
 // Create POST route to create an expense
 router.post("expense/create", create);
