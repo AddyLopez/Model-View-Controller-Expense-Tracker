195a73945e66bf6911bae8c95e9a181c8afc1042
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-08-27 01:25:11 -0600

    Update database.js

--- starting-code/models/database.js
+++ starting-code/models/database.js
@@ -3,6 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
